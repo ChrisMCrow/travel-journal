53147f8c20a0dc080280e879427807c10fa64246
Chris Crow <chrismcrow@gmail.com>
2018-08-20 09:35:06 -0700

    build html and first back-end function

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// back-end
+function TravelEntry(country, cities, date) {
+  this.country = country;
+  this.cities = cities;
+  this.date = date;
+}
+
+
+// front-end
+$(document).ready(function() {
+  $("form#new-entry").submit(function(event) {
+    event.preventDefault();
+
+    
+  });
+});
